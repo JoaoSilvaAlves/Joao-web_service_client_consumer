cf9ae950d256897b8b1536397d71122b58b572dc
JoaoSilvaAlves <Joao.SilvaAlves@rpn.ch>
2017-01-31 14:21:52 +0100

    Fonctionnalités de base du site opérationnelles

--- web_meteo/index.php
+++ web_meteo/index.php
@@ -4,44 +4,84 @@ $client = new ApiSimpleGetRestClient('http://www.prevision-meteo.ch/services/jso
 $response = $client->get('Neuchâtel');
 $data = json_decode(($response), True);
 
-if (isset($_POST["dropDown_DaysNumber"])) {
-    
+
+//Vérification d'ajout de ville pour création de cookie
+if (isset($_POST["v_CityToAdd"])) {
+    CreateCookie($_POST["v_CityToAdd"]);
 }
 
-function VerifCityExists() {
+//Création de nouveau cookie avec comme nom et contenu le nom de la ville
+//Appelé depuis VerifyExists()
+function CreateCookie($CityName) {
     if (isset($_POST["v_CityToAdd"])) {
-        AddCityToList($_POST["v_CityToAdd"]);
+        setcookie($CityName, $CityName, (time() + 86400 * 365));
+        header("Refresh:0");
     }
 }
 
-function AddCityToList($CityToAdd)
-{
-    echo '<label>';
-    echo '<input="checkbox" name="check_list[""value="' . $CityToAdd . '/>' . $CityToAdd . '</label>';
+//Vérification que la ville à ajouter existe bien et soit accessible depuis le service web
+function CallCookie() {
+    foreach ($_COOKIE as $VilleEnregistree) {
+        AddCityToList($VilleEnregistree);
+    }
+}
+
+//Ajout de la ville dans la liste déroulante
+function AddCityToList($CityToAdd) {
+    if (isset($_COOKIE)) {
+        echo '<label>';
+        echo '<input type="checkbox" name="check_list[]" value="' . $CityToAdd . '"/> ' . $CityToAdd . '</label>';
+    }
 }
 
 //Test d'existence, si variable vide et si le contenu ne dépasse pas 6 objets,
 //appel de fonction qui ira construire les zones d'affichage pour la météo
-function CallForTables() {
-    if (isset($_POST['check_list'])) {
-        if (!empty($_POST['check_list'])) {
-            if (count($_POST['check_list']) <= 6) {
-                foreach ($_POST['check_list'] as $check) {
-                    AddCityForecast($check, $_POST["dropDown_DaysNumber"]);
+try {
+
+    function CallForTables() {
+        if (isset($_POST['check_list'])) {
+            if (!empty($_POST['check_list'])) {
+                if (count($_POST['check_list']) <= 6) {
+                    foreach ($_POST['check_list'] as $check) {
+                        AddCityForecast($check, $_POST["dropDown_DaysNumber"]);
+                    }
+                } else {
+                    echo 'Vous ne pouvez choisir que 6 villes au même temps.';
                 }
             } else {
-                echo 'Vous ne pouvez choisir que 6 villes au même temps.';
+                echo 'Veuillez sélectionner des villes à afficher' . '</br>';
             }
         } else {
-            echo 'Veuillez sélectionner des villes à afficher' . '</br>';
+            
         }
-    } else {
-        
     }
+
+} catch (Exception $e) {
+    echo "erreur dans l'affichage des tables";
 }
 
 function AddCityForecast($NameCity, $NumberDays) {
-    
+    $client = new ApiSimpleGetRestClient('http://www.prevision-meteo.ch/services/json');
+    $responseAdd = $client->get($NameCity);
+    $data = json_decode(($responseAdd), True);
+
+    echo '<div class="col-md-4"><div class="row"><table class="tg"><tr>';
+    echo '<th class="tg-baqh" colspan="4">Météo ' . $data['city_info']['name'] . '</th></tr>';
+    echo "<tr><td class='tg-baqh' colspan='4'>Aujourd'hui</td></tr>";
+
+    echo '<tr><td class="tg-yw4l">' . $data['fcst_day_0']['tmin'] . '</td>';
+    echo '<td class="tg-yw4l" colspan="3" rowspan="2">' . '<img src="' . $data['current_condition']['icon'] . '"</img></td></tr>';
+    echo '<tr><td class="tg-yw4l">' . $data['fcst_day_0']['tmax'] . '</td></tr>';
+
+    if ($NumberDays != 1) {
+        for ($i = 1; $i < $NumberDays; $i++) {
+            echo '<tr><td class="tg-yw4l">' . $data['fcst_day_'.$i.'']['day_long'] . '</td>';
+            echo '<td class="tg-yw4l">' . $data['fcst_day_'.$i.'']['tmin'] . '</td>';
+            echo '<td class="tg-yw4l">' . $data['fcst_day_'.$i.'']['tmax'] . '</td>';
+            echo '<td class="tg-yw4l">'.'<img src="' . $data['fcst_day_'.$i.'']['icon'] . '"</img></td></tr>';
+        }
+    }
+    echo '</div></div></table></br>';
 }
 ?>
 
@@ -62,6 +102,14 @@ function AddCityForecast($NameCity, $NumberDays) {
     }
     //document.cookie = sChaine;
 </script>
+<style>
+    .tg  {border-collapse:collapse;border-spacing:0;border-width:2px;border-style:solid; margin-left: 10px;text-align: center;}
+    .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 16px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+    .tg th{font-family:Arial, sans-serif;font-size:18px;font-weight:normal;padding:10px 16px;border-style:solid;border-width:2px;overflow:hidden;word-break:normal;text-align: center;font-weight: bold;}
+    .tg .tg-jsj9{font-size:16px;font-family:"Comic Sans MS", cursive, sans-serif !important;;text-align:center;vertical-align:top}
+    .tg .tg-by1k{font-weight:bold;font-size:18px;font-family:"Comic Sans MS", cursive, sans-serif !important;;text-align:center;vertical-align:top}
+    .tg .tg-yw4l{vertical-align:top}
+</style>
 
 <html>
     <header>
@@ -80,12 +128,12 @@ function AddCityForecast($NameCity, $NumberDays) {
                 <div class="col-md-12">
                     <form class="formVilles" method="post">
                         <div class="col-md-3">
-                            <label for="ajouterVille">
+                            <label for="AddCityToShow">
                                 Ville(s) à afficher
                             </label>
                         </div>
 
-                        <label for="ajouterVille">
+                        <label for="DaysNumber">
                             Nombre de jours
                         </label>
 
@@ -95,7 +143,7 @@ function AddCityForecast($NameCity, $NumberDays) {
 
                                     <div class="overSelect dropdown"></div>
                                     <select>
-                                        <option></option>
+                                        <option>Sélectionnez</option>
                                     </select>
                                 </div>
                                 <div>
@@ -109,7 +157,7 @@ function AddCityForecast($NameCity, $NumberDays) {
                                         <label>
                                             <input type="checkbox"  name="check_list[]" value="Lausanne"/> Lausanne</label>
                                         <?php
-                                        VerifCityExists();
+                                        CallCookie();
                                         ?>
                                     </div>
                                 </div>
@@ -131,8 +179,6 @@ function AddCityForecast($NameCity, $NumberDays) {
                         <div class="col-md-2">
                             <button type="submit" class="btn btn-default"> Recharger </button>
                         </div>
-
-
                     </form>
                 </div>
             </div>
@@ -145,7 +191,7 @@ function AddCityForecast($NameCity, $NumberDays) {
                     <form role="form" method="post">
                         <div class="form-group">
                             <div>
-                                <label for="ajouterVille">
+                                <label for="AddCityToList">
                                     Ville à ajouter à la liste
                                 </label>
                             </div>
@@ -163,14 +209,14 @@ function AddCityForecast($NameCity, $NumberDays) {
             </div>
         </div>
 
-        <!-- Enlever ville de la liste déroulante, il faut entrer le nom correct de la ville !-->
+        <!-- Enlever ville de la liste déroulante, il faut entrer le nom correct de la ville 
         <div class="col-md-12">
             <div class="row">
                 <div class="col-md-12">
                     <form role="form" method="post">
                         <div class="form-group">
                             <div>
-                                <label for="enleverVille">
+                                <label for="deleteCity">                          TODO
                                     Ville à enlever de la liste
                                 </label>
                             </div>
@@ -185,7 +231,7 @@ function AddCityForecast($NameCity, $NumberDays) {
                 </div>
             </div>
         </div>
-
+        !-->
         <!-- Partie affichage des données météo pour les différentes villes sélectionnées précédemment !--> 
         <div class="col-md-12 ">
             <hr class="traitHorizontal">
@@ -194,58 +240,32 @@ function AddCityForecast($NameCity, $NumberDays) {
         <?php
         CallForTables();
         ?>
-        <style>
-            .tg  {border-collapse:collapse;border-spacing:0;border-width:2px;border-style:solid; margin-left: 10px;text-align: center;}
-            .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 16px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
-            .tg th{font-family:Arial, sans-serif;font-size:18px;font-weight:normal;padding:10px 16px;border-style:solid;border-width:2px;overflow:hidden;word-break:normal;text-align: center;font-weight: bold;}
-            .tg .tg-jsj9{font-size:16px;font-family:"Comic Sans MS", cursive, sans-serif !important;;text-align:center;vertical-align:top}
-            .tg .tg-by1k{font-weight:bold;font-size:18px;font-family:"Comic Sans MS", cursive, sans-serif !important;;text-align:center;vertical-align:top}
-            .tg .tg-yw4l{vertical-align:top}
-        </style>
 
-        <div class="col-md-12 ">
-            <div class="row">
-                <table class="tg">
-                    <tr>
-                        <th class="tg-baqh" colspan="4"><?php echo 'Météo ' . $data['city_info']['name'] ?></th>
-                    </tr>
-                    <tr>
-                        <td class="tg-baqh" colspan="4">Aujourd'hui</td>
-                    </tr>
-                    <tr>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_0']['tmin'] ?></td>
-                        <td class="tg-yw4l" colspan="3" rowspan="2"><?php echo '<img src="' . $data['current_condition']['icon'] . '"</img>' ?></td>
-                    </tr>
-                    <tr>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_0']['tmax'] ?></td>
-                    </tr>
-                    <tr>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_1']['day_long'] ?></td>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_1']['tmin'] ?></td>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_1']['tmax'] ?></td>
-                        <td class="tg-yw4l"><?php echo '<img src="' . $data['fcst_day_1']['icon'] . '"</a>' ?></td>
-                    </tr>
-                    <tr>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_2']['day_long'] ?></td>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_2']['tmin'] ?></td>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_2']['tmax'] ?></td>
-                        <td class="tg-yw41"><?php echo '<img src="' . $data['fcst_day_2']['icon'] . '" style="width:100%"</a>' ?></td>
-                    </tr>
-                    <tr>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_3']['day_long'] ?></td>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_3']['tmin'] ?></td>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_3']['tmax'] ?></td>
-                        <td class="tg-yw4l"><?php echo '<img src="' . $data['fcst_day_3']['icon'] . '"</a>' ?></td>
-                    </tr>
-                    <tr>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_4']['day_long'] ?></td>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_4']['tmin'] ?></td>
-                        <td class="tg-yw4l"><?php echo $data['fcst_day_4']['tmax'] ?></td>
-                        <td class="tg-yw4l"><?php echo '<img src="' . $data['fcst_day_4']['icon'] . '"</a>' ?></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
+        <!--        <div class="col-md-12 ">
+                    <div class="row">
+                        <table class="tg">
+                            <tr>
+                                <th class="tg-baqh" colspan="4"><?php //echo 'Météo ' . $data['city_info']['name']   ?></th>
+                            </tr>
+                            <tr>
+                                <td class="tg-baqh" colspan="4">Aujourd'hui</td>
+                            </tr>
+                            <tr>
+                                <td class="tg-yw4l"><?php //echo $data['fcst_day_0']['tmin']   ?></td>
+                                <td class="tg-yw4l" colspan="3" rowspan="2"><?php //echo '<img src="' . $data['current_condition']['icon'] . '"</img>'   ?></td>
+                            </tr>
+                            <tr>
+                                <td class="tg-yw4l"><?php //echo $data['fcst_day_0']['tmax']   ?></td>
+                            </tr>
+                            <tr>
+                                <td class="tg-yw4l"><?php //echo $data['fcst_day_1']['day_long']   ?></td>
+                                <td class="tg-yw4l"><?php //echo $data['fcst_day_1']['tmin']   ?></td>
+                                <td class="tg-yw4l"><?php //echo $data['fcst_day_1']['tmax']   ?></td>
+                                <td class="tg-yw4l"><?php //echo '<img src="' . $data['fcst_day_1']['icon'] . '"</a>'   ?></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>-->
     </body>
     <footer>
 
